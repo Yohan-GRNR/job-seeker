80b485b6f44734011d40d0781376bf6658dfc8f2
Yohan Grenier <y.grenier01@gmail.com>
2024-08-27 20:43:12 +0200

    Update - Add google

--- job-seeker-streamlit.py
+++ job-seeker-streamlit.py
@@ -83,7 +83,7 @@ if st.sidebar.button("Let's GO !"):
             "api_key": api_key,
             "device": "desktop",
             "engine": "google_jobs",
-            # "google_domain": "google.com",
+            "google_domain": "google.com",
             "q": search_term,
             # "hl": "en", # Language parameter can return No result
             # "gl": domain_country,  # Domain of country
